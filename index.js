5c36c71a0223c2c4335c2255e9a78cdb150fc666
prabhu_b <prabhubarathi@gmail.com>
2018-07-30 09:52:08 -0400

    updates

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 const SlackBot = require('slackbots') ;
 const axios = require('axios');
 const authtoken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ2bXdhcmUuY29tOjRlYTE0N2ExLWVkNWQtNGE1OC05MjBlLThhNDMyZmFhMTA4YSIsImF6cCI6ImNzcF9wcmRfZ2F6X2ludGVybmFsX2NsaWVudF9pZCIsImRvbWFpbiI6InZtd2FyZS5jb20iLCJjb250ZXh0IjoiZjZjZmY1MjgtMTNjMC00ODBlLTgzOTctMDQ0NDk5ZjU2ZDI4IiwiaXNzIjoiaHR0cHM6Ly9nYXouY3NwLXZpZG0tcHJvZC5jb20iLCJwZXJtcyI6WyJleHRlcm5hbC9aRjh1YU5yY3dtdmtSX1l6Q2hQSmpOZ3dLTjhfL25zeDpjbG91ZF9zZXJ2aWNlX2FkbWluIiwiZXh0ZXJuYWwvWkY4dWFOcmN3bXZrUl9ZekNoUEpqTmd3S044Xy9uc3g6Y2xvdWRfc2VydmljZV9hdWRpdG9yIiwiZXh0ZXJuYWwvbzNlY2JzQXZqcHc2bG1MM2FsaUpYMjl6VmhFXy92a2U6c2VydmljZS11c2VyIiwiZXh0ZXJuYWwvN2NKMm5nU19oUkNZX2JJYld1Y000S1dRd09vXy9sb2ctaW50ZWxsaWdlbmNlOnVzZXIiLCJleHRlcm5hbC9Zdy1IeUJlUXpqQ1hrTDJ3UVNlR3dhdUotbUFfL2NhdGFsb2c6dXNlciIsImNzcDpvcmdfbWVtYmVyIiwiZXh0ZXJuYWwvWnk5MjRtRTNkd24yQVN5VlpSME5uN2x1cGVBXy9hdXRvbWF0aW9uc2VydmljZTp1c2VyIiwiZXh0ZXJuYWwvOXFqb05hZkRwOVhreXlRTGNMQ0tXUHNBaXIwXy92cm5pOmFkbWluIiwiZXh0ZXJuYWwvN2NKMm5nU19oUkNZX2JJYld1Y000S1dRd09vXy9sb2ctaW50ZWxsaWdlbmNlOmFkbWluIiwiZXh0ZXJuYWwvdWx2cXRONDE0MWJlQ1Qyb09uYmotd2xrekdnXy9Db2RlU3RyZWFtOmRldmVsb3BlciIsImV4dGVybmFsL1NvQ0QzMjZkWS10R0JzTGFKZjRBSEVzbmFXMF8vZGlzY292ZXJ5OnVzZXIiLCJleHRlcm5hbC9sR29ucl9sbkJIeXdxS1BuMzJROFVmMjJuallfL2Rpc2NvdmVyeTp1c2VyIl0sImNvbnRleHRfbmFtZSI6ImJmZDdhNzUxLWIyZjUtNDNhMy1iMmIzLTBjODJkNjg5NmFlZSIsImV4cCI6MTUzMjcyNjA5MywiaWF0IjoxNTMyNzI0MjkzLCJqdGkiOiJmMzk3MTA5OS1jM2VhLTQxMTktYTQwYi1mYWQ5OGJiNmE5YzIiLCJ1c2VybmFtZSI6InBiYXJhdGhpIn0.TxZgvi6qsyLdEkkOG6MQWyZFuET1qF-TXcvlAtXe_USWNX6ZU7bVgIQdfaWwKk_x2ffm17fFJXg3MRFlvjqC5TNu12SYue_c0AbqahJJvleXlqjTsPZ_eq-XqC0wrmK1M0sljaq1Oi1YYIxzhyB-GEQTk0xR6hJ3bwSLsQVZ7ychtekkD1F3S4xn49JH48zhnjOWLxFjv0GqK14zL13HQNb_a8fKlsfqIHogPpAwB2oTbg-OczPJW_uOIBcfHws66i0g8anEMICal2DFhFNXBQ-YJdPaD3b3qLpa3sFC1_u2YkX9CVK_AOXzCciqEOiHjEBhVTxNrc8ml5-0IAG9iQ'
-var iplookup = require('iplookup')
+
+var Botkit = require('botkit')
+var Witbot = require('witbot')
+
+var witToken = 'PQNONQM2GYW2CJNHWZ2YULIJE2ZGHXRC'
+var slackToken = 'xoxb-386509464003-401409504435-gNZiKPgqCEXoJwfDI9nniyAU'
+
+var witbot = Witbot(witToken)   
+
+var controller = Botkit.slackbot({
+    debug: false
+})
+
+controller.spawn({ token: slackToken }).startRTM(function (err, bot, payload) {
+    if (err) throw new Error('Error connecting to Slack: ', err)
+    console.log('Connected to Slack')
+  }) 
+
+// wire up DMs and direct mentions to 
+
+controller.hears('hello','direct_message,direct_mention', function(bot, message) {
+    bot.reply(message,'Hello yourself!')
+})
+
+controller.hears(['pizzatime'],['ambient','direct_message'],function(bot,message) {
+    bot.startConversation(message, askFlavor);
+  })
+  
+  askFlavor = function(response, convo) {
+    convo.ask("What flavor of pizza do you want?", function(response, convo) {
+      convo.say("Awesome.");
+      askSize(response, convo);
+      convo.next();
+    });
+  }
+  askSize = function(response, convo) {
+    convo.ask("What size do you want?", function(response, convo) {
+      convo.say("Ok.")
+      askWhereDeliver(response, convo);
+      convo.next();
+    });
+  }
+  askWhereDeliver = function(response, convo) { 
+    convo.ask("So where do you want it delivered?", function(response, convo) {
+      convo.say("Ok! Goodbye.");
+      convo.next();
+    });
+  }
+
+
+  
 
 const bot = new SlackBot ({
-    token: 'xoxb-386509464003-401409504435-gNZiKPgqCEXoJwfDI9nniyAU' ,
+    token: slackToken ,
     name: 'jarvis'
 })
 
@@ -13,7 +63,6 @@ bot.on('start', () => {
         icon_emoji: ':smiley:'
     }
     bot.postMessageToChannel('general','Hey there, Jarvis Here! What can i do?',params);
-
 }
 
 );
@@ -42,9 +91,6 @@ function handleMessage(message) {
     if(message.includes(' createnewapp')){
         apptier();
     }
-    if(message.includes(' iplookup')){
-        iplookup();
-    }
 
 }
 
@@ -60,19 +106,6 @@ function chuckjoke() {
     )
 }
 
-
-function namelookup() {
-    var config = {
-        headers: {'csp-auth-token': authtoken}
-    };
-    axios.get('https://www.mgmt.cloud.vmware.com/ni/api/ni/entities/vms/10202:601:1856511441', config)
-        .then(res => {
-            const entity1 = res.data.name;
-        bot.postMessageToChannel('general',`${entity1}`);
-    }
-)
-}
-
 function iplookup() {
     var config = {
         headers: {
@@ -86,8 +119,6 @@ function iplookup() {
         bot.postMessageToChannel('general',"Your Object ID = "+`${iplookup}`);
         }
     )
-
 }
-
 
 
